e3e7487677d0bbd4ea9314619e30954d5ad07758
Bhavik <bhaviktembhare2@gmail.com>
2024-03-14 00:48:07 +0530

    Done all the Cart functionality

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,6 +1,64 @@
+import { Button, Col, Image, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import { CartState } from "../Context/Context";
+import { useEffect, useState } from "react";
+import Rating from './Rating.jsx';
+import { AiFillDelete } from "react-icons/ai";
+
 const Cart = () => {
+
+  const { state: { cart }, dispatch } = CartState();
+
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0))
+  }, [cart])
+
   return (
-    <div>Cart</div>
+    <div className="home">
+      <div className="productContainer">
+        <ListGroup>
+          {
+            cart.map((prod) => (
+              <ListGroupItem key={prod.id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={prod.image} alt={prod.name} fluid rounded />
+                  </Col>
+                  <Col md={2}>
+                    <span>{prod.name}</span>
+                  </Col>
+                  <Col md={2}> ₹ {prod.price}</Col>
+                  <Col md={2}>
+                    <Rating rating={prod.rating} />
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => dispatch({
+                        type: "REMOVE_FROM_CART",
+                        payload: prod,
+                      })}
+                    >
+
+                      <AiFillDelete fontSize="20px " />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+            ))
+          }
+        </ListGroup>
+      </div>
+      <div className="filters summery">
+        <span className="title">Subtotal({cart.length})items</span>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>Total:₹ {total}</span>
+        <Button type="button" disabled={cart.length === 0}>
+          Proceed to Checkout
+        </Button>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,21 @@
-import { Container, FormControl, Navbar, NavbarBrand, NavbarText,Dropdown,Badge } from "react-bootstrap";
+import { Container, FormControl, Navbar, NavbarBrand, NavbarText, Dropdown, Badge, Button } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { CartState } from "../Context/Context";
+import { AiFillDelete } from "react-icons/ai";
 
 
 const Header = () => {
+
+    const {
+        state: { cart },
+        dispatch
+    } = CartState();
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavbarBrand>
-                    <Link href="/">Shopping Cart</Link>
+                    <Link to="/">Shopping Cart</Link>
                 </NavbarBrand>
                 <NavbarText className="search">
                     <FormControl
@@ -18,14 +25,43 @@ const Header = () => {
                     />
 
                 </NavbarText>
-                <Dropdown  align="end">
+                <Dropdown align="end">
                     <Dropdown.Toggle variant="success" id="dropdown-basic">
                         <FaShoppingCart color="white" fontSize="25px" />
-                        <Badge>{10}</Badge>
+                        <Badge>{cart.length}</Badge>
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu style={{ minWidth: 370 }}>
-                       <span style={{ padding:10 }}>Cart is Empty!</span>
+                        {
+                            cart.length > 0 ? (
+                                <>
+                                    {
+                                        cart.map((prod) => (
+                                            <span className="cartitem" key={prod.id}>
+                                                <img src={prod.image} className="cartItemImg" alt={prod.name} />
+                                                <div className="cartItemDetail">
+                                                    <span>{prod.name}</span>
+                                                    <span>₹ {prod.price.split(".")[0]}</span>
+                                                </div>
+                                                <AiFillDelete
+                                                    fontSize="20px"
+                                                    style={{ cursor: "pointer" }}
+                                                    onClick={() => dispatch({
+                                                        type: "REMOVE_FROM_CART",
+                                                        payload: prod
+                                                    })} />
+                                            </span>
+                                        ))
+                                    }
+                                    <Link to="/cart">
+                                        <Button style={{ width: "95%", margin: "0 10px" }}>
+                                            Go to Cart
+                                        </Button>
+                                    </Link>
+                                </>
+                            ) : (<span style={{ padding: 10 }}>Cart is Empty!</span>)
+                        }
+
                     </Dropdown.Menu>
                 </Dropdown>
             </Container>
